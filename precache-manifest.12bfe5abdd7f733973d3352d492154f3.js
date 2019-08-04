b8dbb2d7f89001ed2fc8a37fe5acf435efede96f
dingq84 <dingq84@gmail.com>
2019-08-04 23:48:04 +0800

    Updates

---
+++ precache-manifest.12bfe5abdd7f733973d3352d492154f3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2afce41f264d4b8cdebcd23beb5e97",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "b1298996221d97389ca9",
+    "url": "/music-player/static/css/main.b786954d.chunk.css"
+  },
+  {
+    "revision": "7ad67ae5803ef9e7303a",
+    "url": "/music-player/static/js/2.0f60707b.chunk.js"
+  },
+  {
+    "revision": "b1298996221d97389ca9",
+    "url": "/music-player/static/js/main.6564b199.chunk.js"
+  },
+  {
+    "revision": "ae1bece6dc169c4ab370",
+    "url": "/music-player/static/js/runtime~main.fdd2de90.js"
+  },
+  {
+    "revision": "2d0ca51830c917b47da2038b5f66cc78",
+    "url": "/music-player/static/media/A_Ghost_Town.2d0ca518.mp3"
+  },
+  {
+    "revision": "e2d756c99744d40f114214c46505decc",
+    "url": "/music-player/static/media/Component-1.e2d756c9.svg"
+  },
+  {
+    "revision": "a6cea858173abe9474e6debe7d33df48",
+    "url": "/music-player/static/media/Component.a6cea858.svg"
+  },
+  {
+    "revision": "bf14fa028ca2bc3850aa79502867a593",
+    "url": "/music-player/static/media/Down.bf14fa02.mp3"
+  },
+  {
+    "revision": "c16c35d592377e4b6d906be443027ea8",
+    "url": "/music-player/static/media/For_We_Are_Many.c16c35d5.mp3"
+  },
+  {
+    "revision": "3e3ce561a158b503e5965921ddc254a5",
+    "url": "/music-player/static/media/Ice_Cream.3e3ce561.mp3"
+  },
+  {
+    "revision": "f283d3f087841106808d0e719418bcf6",
+    "url": "/music-player/static/media/Images_of_Tomorrow.f283d3f0.mp3"
+  },
+  {
+    "revision": "e56b6292f2c20bf21a95a3fadc771aad",
+    "url": "/music-player/static/media/Jazz_Mango.e56b6292.mp3"
+  },
+  {
+    "revision": "4ff034f3f088d65484fa5edf444f814a",
+    "url": "/music-player/static/media/Keys_to_the_Apocalypse.4ff034f3.mp3"
+  },
+  {
+    "revision": "8162f477554645bd3a83f6c2a058af88",
+    "url": "/music-player/static/media/Song_of_Mirrors.8162f477.mp3"
+  },
+  {
+    "revision": "32ba9d3d9a70b50010e2639161aee157",
+    "url": "/music-player/static/media/Space_Hunter.32ba9d3d.mp3"
+  },
+  {
+    "revision": "34d015a28bf4ed31fb4d64da0a33eed6",
+    "url": "/music-player/static/media/The_New_King.34d015a2.mp3"
+  },
+  {
+    "revision": "b21837578ec5ec5bc1b66e34c38bbced",
+    "url": "/music-player/static/media/archive-2.b2183757.svg"
+  },
+  {
+    "revision": "e321f770986bb963ea47dfc52646798c",
+    "url": "/music-player/static/media/astist_photo.e321f770.png"
+  },
+  {
+    "revision": "b390caa1d74cfccf4d8662ca9184bd45",
+    "url": "/music-player/static/media/heart-2-pink.b390caa1.svg"
+  },
+  {
+    "revision": "689f2425597d1537027bbe44f26b0ad1",
+    "url": "/music-player/static/media/heart-2.689f2425.svg"
+  },
+  {
+    "revision": "30ca485924f2bd70344669450a9726fa",
+    "url": "/music-player/static/media/home-52.30ca4859.svg"
+  },
+  {
+    "revision": "c56679c9ccf5441649539b64a29b15ba",
+    "url": "/music-player/static/media/ic_library_music_48px.c56679c9.svg"
+  },
+  {
+    "revision": "bbed3d14a48b4cf92cc941986f22b589",
+    "url": "/music-player/static/media/ic_music_note_24px.bbed3d14.svg"
+  },
+  {
+    "revision": "5d9fc74e86be6ef68dad7b1ebdc81da4",
+    "url": "/music-player/static/media/ic_repeat_24px.5d9fc74e.svg"
+  },
+  {
+    "revision": "26a9928d23672c31d007c2d6d74b4e77",
+    "url": "/music-player/static/media/ic_shuffle_24px.26a9928d.svg"
+  },
+  {
+    "revision": "a782b37911a1cd4c5532ea506aeb5aff",
+    "url": "/music-player/static/media/ic_volume_up_24px.a782b379.svg"
+  },
+  {
+    "revision": "e5b4b1063d1fb59f5ca82812aa52feb0",
+    "url": "/music-player/static/media/pause.e5b4b106.svg"
+  },
+  {
+    "revision": "2de02886e286be823e7f3c36349e9bbc",
+    "url": "/music-player/static/media/settings-gear-63.2de02886.svg"
+  },
+  {
+    "revision": "15685a27cdca50f08840cd7089b8151f",
+    "url": "/music-player/static/media/zoom-2.15685a27.svg"
+  }
+]);
